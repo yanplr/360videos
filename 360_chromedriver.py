6d0202dd236b3af7c81dd203d69b0d880cb7ab8e
yanplr <857103939@qq.com>
2022-08-17 04:50:19 +0800

    Update 360_chromedriver.py

--- 360_chromedriver.py
+++ 360_chromedriver.py
@@ -58,7 +58,8 @@ def getcookies():
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
-    options.add_experimental_option("detach", True)
+#     options.add_experimental_option("detach", True)
+    options.set_capability("detach", True)
     ## 部署到github action时删除debugger_address
     options.debugger_address = '127.0.0.1:9222'
     d = DesiredCapabilities.CHROME
