4ece6b9a5af4b870a60c8b5eaa54458f349b51e7
yanplr <857103939@qq.com>
2022-08-19 15:58:53 +0800

    captcha

--- 360videoDowoload.py
+++ 360videoDowoload.py
@@ -100,7 +100,7 @@ def getcookies():
     # ocr = ''
     while(flag and tryTime < 30):
         try:
-            user_name = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div/div[3]/a[1]')
+            user_name = driver.find_elements(By.XPATH, '/html/body/div[1]/div[2]/div/div[3]/a[1]')
             print(f'用户名：{user_name.text}')
             print(f'登录成功')
             flag = False
